2761204c5edd78d8f561f981862e92ceb9c54c79
urvishkapadiya <kapadiyaurvish16@gmail.com>
2025-08-20 18:30:45 -0400

    feat(root): invoke vpc module from root with CIDR and subnets

--- variables.tf
+++ variables.tf
@@ -3,3 +3,21 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
