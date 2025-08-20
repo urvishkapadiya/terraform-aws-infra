e09e14b866f86a865fbb4b6db2f8e91ae4449c26
urvishkapadiya <kapadiyaurvish16@gmail.com>
2025-08-20 18:16:12 -0400

    feat(vpc): add private subnet resource

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -30,6 +30,18 @@ resource "aws_subnet" "public" {
   }
 }
 
+resource "aws_subnet" "private" {
+  count                   = length(var.private_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private_subnet_cidrs[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = false
+  
+  tags = {
+    Name = "private-subnet-${count.index + 1}"
+  }
+}
+
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -7,3 +7,8 @@ variable "public_subnet_cidrs" {
   description = "CIDR blocks for public subnets"
   type        = list(string)
 }
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
