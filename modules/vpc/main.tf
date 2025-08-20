1f47e06c8e1b4deb0a6a49ef7350e844ee7151fc
urvishkapadiya <kapadiyaurvish16@gmail.com>
2025-08-20 18:23:59 -0400

    feat(vpc): add NAT gateways and private route table with associations

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -60,3 +60,43 @@ resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_eip" "nat" {
+  count = length(var.public_subnet_cidrs)
+  vpc   = true
+  
+  tags = {
+    Name = "nat-eip-${count.index + 1}"
+  }
+}
+
+resource "aws_nat_gateway" "main" {
+  count         = length(var.public_subnet_cidrs)
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+ 
+  tags = {
+    Name = "nat-${count.index + 1}"
+  }
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_route_table" "private" {
+  count  = length(var.private_subnet_cidrs)
+  vpc_id = aws_vpc.main.id
+  
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main[count.index].id
+  }
+
+  tags = {
+    Name = "private-rt-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -12,3 +12,8 @@ output "public_subnet_ids" {
   description = "IDs of the public subnets"
   value       = aws_subnet.public[*].id
 }
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = aws_subnet.private[*].id
+}
